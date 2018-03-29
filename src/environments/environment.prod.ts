08fd2b555f13fca0ef866c99d0f622f14670d5be
rattasat <rattasat.rtskt@gmail.com>
2018-03-29 07:08:13 +0700

    add friend button

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   envName: 'prod',
   apiUrl: 'https://petbookthaiapi.herokuapp.com',
-  lineFri: 'href="https://line.me/R/ti/p/%40ent0255w'
+  lineFri: 'https://line.me/R/ti/p/%40ent0255w'
 };
